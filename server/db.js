24d2b9143fe334aab48383f85ce46a6d5a6c733c
Tin Nguyen <tinytheyfon8@users.noreply.github.com>
2019-03-11 13:16:03 -0500

    Added file for database login

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const host = 'localhost'
+const user = 'insert db username here'
+const password = 'insert db password here'
+const database = 'insert database name here'
+
+module.exports = {
+  host: host,
+  user: user,
+  password: password,
+  database: database
+}
