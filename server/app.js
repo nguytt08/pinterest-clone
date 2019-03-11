e697e0428da45063717af2194fdaad1f14f3e8a1
tinytheyfon8 <tinytheyfon8@gmail.com>
2019-03-11 14:32:10 -0500

    Front end takes in links from database and renders photos

--- server/app.js
+++ server/app.js
@@ -3,7 +3,31 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const database = require('./db');
+const cors = require('cors');
 
+app.use(cors());
+
+const connection = mysql.createConnection(database);
+connection.connect();
+
+let links;
+
+connection.query('SELECT url FROM urlLinks', function(err, results) {
+  if (err) throw err
+
+  links = results;
+  for (var i = 0; i < results.length; i++) {
+  console.log('Results: ' + results[i].url)
+  }
+
+})
+
+connection.end();
+
+app.get('/links', function (req, res) {
+  res.send(links);
+})
 
 app.get('/', function (req, res) {
   res.send("/ Path WORKS");
--- src/App.js
+++ src/App.js
@@ -3,26 +3,47 @@ import './App.css';
 import PictureCard from './components/PictureCard.js';
 import LinkForm from './components/LinkForm.js';
 
+import axios from 'axios';
+
 class App extends Component {
   state = {
-    inputLink: 'https://media.istockphoto.com/photos/student-searching-books-picture-id623200866'
+    inputLink: 'https://media.istockphoto.com/photos/student-searching-books-picture-id623200866',
+    links: []
   }
 
   // https://images.pexels.com/photos/1120162/pexels-photo-1120162.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940
 
-handleInputChange = (e) => {
-  this.setState({inputLink : e.target.value});
-}
+  componentDidMount() {
+    axios.get(`http://localhost:4000/links`)
+    .then(res => {
+      console.log('component did mount: ' + JSON.stringify(res.data));
+      console.log('type: ' + typeof(res.data));
+      const links = res.data;
+      this.setState( {links: links} );
+    })
+  }
+
+  handleInputChange = (e) => {
+    this.setState({inputLink : e.target.value});
+  }
 
 
-handleSubmit = (link) => {
-  console.log("handle submit link: " + link);
-  this.setState({
-    url: link
-  })
+  handleSubmit = (link) => {
+    console.log("handle submit link: " + link);
+    // this.setState({
+    //   url: link
+    // })
 
-  console.log(this.state);
-}
+    // this.state.links.map(link => {
+    //   console.log('link map: ' + link);
+    // })
+
+    // this.state.links
+    console.log("current state: " + (this.state.links[0].url));
+
+    // this.state.links.map(link => console.log('linksss: ' + link));
+    // console.log('type of: ' + this.state.links[0]);
+  }
 
   render() {
     return (
@@ -33,6 +54,10 @@ handleSubmit = (link) => {
           <input type="submit" value="Submit" onClick={this.handleSubmit}/>
         </div>
         <PictureCard link={this.state.inputLink}/>
+        {this.state.links.map(link => {
+          return <PictureCard link={link.url} />
+        })
+      }
       </div>
     );
   }
